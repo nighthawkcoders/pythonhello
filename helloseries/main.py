519fa93c8e17c3a8c8bab5f830d33482ce0abf77
John Mortensen <jmortensen@powayusd.com>
2020-08-24 19:36:31 -0700

    Name changes

--- helloseries/main.py
+++ helloseries/main.py
@@ -1,17 +1,20 @@
+"""
+  Assignment 1: Extend 5 things with stringy
+  1. make a sentence with punctuation
+  2. manipulate the sentence
+  3. make other words or phrases from sentence
+  4. ... you pick ...
+  5. ... you pick ...
+"""
+# Menu names
+menu_choices = ["Exit", "Strings", "Numbers", "Lists", "Dictionaries"]
 
-menu_choices = ["Exit", "Strings", "Numbers", "Lists","Dictionaries"]
 
-class Selections(object):
-    """
-    Assignment 1: do 5 things with stringy
-    1. make a sentence with punctuation
-    2. manipulate the sentence
-    3. make other words or phrases from sentence
-    4. ... you pick ...
-    5. ... you pick ...
-    """
+# Menu definitions
+class Menu:
+
     def stringy(self):
-        #run stringy.py file
+        # run stringy.py file
         print(menu_choices[1])
         exec(open("./stringy.py").read())
 
@@ -24,47 +27,52 @@ class Selections(object):
     def dicty(self):
         print(menu_choices[4])
 
-def menu():
-    funcs = Selections()
     options = {
-        0: [menu_choices[0], funcs],
-        1: [menu_choices[1], funcs.stringy],
-        2: [menu_choices[2], funcs.numy],
-        3: [menu_choices[3], funcs.listy],
-        4: [menu_choices[4], funcs.dicty]
+        0: [menu_choices[0], None],
+        1: [menu_choices[1], stringy],
+        2: [menu_choices[2], numy],
+        3: [menu_choices[3], listy],
+        4: [menu_choices[4], dicty]
     }
+
+
+# Menu control
+def menu_control():
+    menu = Menu()
+
     repeat: bool = True
     while repeat:
-        #heading section
+        # heading section
         print()
         print("=" * 25)
         print("Please Select An Option")
         print("=" * 25)
 
-        #output menu options
-        items = len(options)
+        # output menu options
+        items = len(menu.options)
         for i in range(0, items):
-            print(str(i) + ": " + options.get(i)[0])
+            print(str(i) + ": " + menu.options.get(i)[0])
 
-        #get choice
+        # get choice
         choice = input("Select Option: ")
 
-        #validation section
-        try:                                #protects/traps errors
-            choice = int(choice)
-            if choice == 0:
-                return                      #exit function
-            elif choice in options:
-                options.get(choice)[1]()    #call function from list
+        # validation section
+        try:  # protects/traps errors
+            choice = int(choice)  # convert input to Integer
+            if choice in menu.options:
+                if menu.options.get(choice)[1] is None:  # 0 condition
+                    return  # exit
+                else:  # valid menu
+                    menu.options.get(choice)[1](menu)  # call function
             else:
-                raise IndexError()          #raise error
-        except ValueError:                  #not a number error
-            print("Not a Number, you need to enter a number.", choice)
+                raise IndexError()  # raise index error
+        except ValueError:  # not a number error
+            print("Not a Number, enter a number.", choice)
             repeat = True
         except IndexError:
-            print("Out of Bounds. Option {0} doesn't exist.".format(choice))
+            print("Out of Bounds. {0} doesn't exist.".format(choice))
             repeat = True
 
 
-#start the display
-menu()
+# start menu
+menu_control()
